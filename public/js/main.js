b8f435d3098aa71cc9a5d7ad06cbac7ea2e142aa
Andrija <andrijagajicbusiness@gmail.com>
2021-03-21 16:27:20 +0100

    Added background and moving circle feature

--- public/js/main.js
+++ public/js/main.js
@@ -2,40 +2,28 @@ const socket = io('http://localhost:3000');
 
 const $joinBtn = document.getElementById('join');
 
-const $canvas = document.querySelector('canvas');
-const ctx = $canvas.getContext('2d');
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.fillStyle = 'red';
-ctx.fill();
-ctx.textAlign = 'center';
-ctx.font = 'sans-serif 18px';
-ctx.fillStyle = 'black';
-ctx.fillText('George13', 95, 50);
-ctx.font = 'sans-serif 12px';
-ctx.fillText('8692', 95, 65);
-
-
-ctx.beginPath();
-ctx.arc(195, 80, 30, 0, 2 * Math.PI);
-ctx.fillStyle = 'green';
-ctx.strokeStyle = '#000';
-ctx.fill();
-ctx.stroke();
-
-ctx.beginPath();
-ctx.arc(175, 120, 20, 0, 2 * Math.PI);
-ctx.fillStyle = 'green';
-ctx.strokeStyle = '#000';
-ctx.fill();
-ctx.stroke();
+let gameState = 'not-started';
 
 $joinBtn.addEventListener('click', () => {
+    gameState = 'playing';
     socket.emit('player-joined', 'ide gas na max');
     const $startDiv = document.querySelector('.start');
     $startDiv.style.display = 'none';
+    document.body.style.overflow = 'unset';
+    $gameDiv = document.querySelector('.game');
+    $gameDiv.style.width = '3000px';
+    $gameDiv.style.height = '3000px';
 })
 
 document.addEventListener('mousemove', e => {
-    // console.log(e.pageX, e.pageY);
+    if (gameState === 'playing') {
+        const $playerCircle = document.querySelector('.circle');
+        $playerCircle.style.left = e.pageX + 'px';
+        $playerCircle.style.top = e.pageY + 'px';
+        // window.scroll({
+        //     top: e.clientX - e.pageX,
+        //     left: e.clientY - e.pageY,
+        //     behavior: 'smooth'
+        // });
+    }
 });
